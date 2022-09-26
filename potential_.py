5748d833f8b09987bc3b27e57412c893fa72ab07
vamsi1961 <cheedellavamsikishore@gmail.com>
2022-09-27 00:37:20 +0530

    half-way done

--- potential_.py
+++ potential_.py
@@ -1,66 +1,126 @@
-from random import random
-import cmath
+
 import numpy as np
 import matplotlib.pyplot as plt
-"""
-def vector (a,b):
-    vec = a-b
 
 
-x_initial = 0
-y_initial = 0
-z_nitial = complex(x_initial,y_initial)
 
-x_goal = 10
-y_goal = 10
-z_goal = complex(x_goal,y_goal)
 
-x = np.arange(-10,10,1)
-y = np.arange(-10,10,1)
-
-X,Y = np.meshgrid(x,y)
-
-delx = np.zeros(X)
-dely = np.zeros(Y)
-
-goal = [50,50]
-obstacle = [25,25] """
 # creating two evenly spaced array with ranging from 
 # -10 to 10
-x = np.arange(-10,10,1)
-y = np.arange(-10,10,1)
 
-# Creating the meshGrid
-X, Y = np.meshgrid(x,y)
 
-# creating the delx and Dely
-delx = np.zeros_like(X)
-dely = np.zeros_like(Y)
-s = 100
+
+
+def add_goal(X,Y,s,r,loc):
+
+  delx = np.zeros_like(X)
+  dely = np.zeros_like(Y)
+
+
+  for i in range(len(x)):
+    for  j in range(len(y)):
+
+      d = np.sqrt((loc[0] - X[i][j])**2 + (loc[1] - Y[i][j])**2)
+      theta = np.arctan2((loc[1] - Y[i][j]),(loc[0] - X[i][j]))
+
+      if (d < r):
+        delx[i][j] = 0
+        dely[i][j] = 0
+
+      elif d > r+s:
+          delx[i][j] = 50*s*np.cos(theta)
+          dely[i][j] = 50*s*np.sin(theta)
+
+      else:
+          delx[i][j] = 50*(d-r)*np.cos(theta)
+          dely[i][j] = 50*(d-r)*np.sin(theta)
+
+  return delx,dely
+
+x = np.arange(0,50,1)
+y = np.arange(0,50,1)
+
+s = 7
 r=2
-for i in range(len(x)):
-  for j in range(len(y)):
-        
-    d= np.sqrt(X[i][j]**2 + Y[i][j]**2)
-    #print(f"{i} and {j}")
-    theta = np.arctan2(Y[i][j],X[i][j])
+seek_points = np.array([[0,0]]) 
+X, Y = np.meshgrid(x,y)
+goal = [40,40]
+delx, dely =add_goal(X, Y,s, r , goal)
 
-    # using the Formula of avoiding obstacle
-    if d< 2:
-      delx[i][j] = np.sign(np.cos(theta))
-      dely[i][j] = np.sign(np.cos(theta))
-    elif d>r+s:
-      delx[i][j] = 0
-      dely[i][j] = 0
-    else:
-      delx[i][j] = 1000*(s+r-d)* np.cos(theta) #it just givrs the component by getting a large value it makes more accurate
-      dely[i][j] = 1000*(s+r-d)*np.sin(theta)
-      print(delx[i][j])
-      print(dely[i][j])
 
-fig, ax = plt.subplots(figsize = (10,10))
-ax.quiver(X, Y, delx, dely)
-ax.add_patch(plt.Circle((0, 0), 2, color='m'))
-#ax.annotate("Obstacle", xy=(0, 0), fontsize=20, ha="center")
-#ax.set_title('Repulsive field of the obstacle')
-plt.show() 
+
+delx , dely = add_goal(X,Y,s,r,goal)
+
+
+def add_obstacle(X,Y,delx,dely,goal):
+  obstacle = [25,25] 
+  s=7
+  r = 3
+  for i in range(len(X)):
+    for j in range(len(Y)):
+      
+      d_goal = np.sqrt((goal[0] - X[i][j])**2 + (goal[1] - Y[i][j])**2)
+      d_obstacle = np.sqrt((obstacle[0] - X[i][j])**2 + (obstacle[1] - Y[i][j])**2)
+      #print(f"{i} and {j}")
+      theta_goal  = np.arctan2((Y[i][j] - goal[1]),(X[i][j] - goal[0]))
+      theta_obstacle = -np.arctan2((Y[i][j] - obstacle[1]),(X[i][j] - obstacle[0]))
+
+      # using the Formula of avoiding obstacle
+      if d_obstacle < r:
+        delx[i][j] = -1*np.sign(np.cos(theta_obstacle))*5 +0
+        dely[i][j] = -1*np.sign(np.cos(theta_obstacle))*5  +0
+      elif d_obstacle>r+s:
+        delx[i][j] += 0 -(50 * s *np.cos(theta_goal))
+        dely[i][j] += 0 - (50 * s *np.sin(theta_goal))
+      elif d_obstacle<r+s :
+        delx[i][j] += -150 *(s+r-d_obstacle)* np.cos(theta_obstacle)
+        dely[i][j] += -150 * (s+r-d_obstacle)*  np.sin(theta_obstacle) 
+      if d_goal <r+s:
+        if delx[i][j] != 0:
+          delx[i][j]  += (50 * (d_goal-r) *np.cos(theta_goal))
+          dely[i][j]  += (50 * (d_goal-r) *np.sin(theta_goal))
+        else:
+          
+          delx[i][j]  = (50 * (d_goal-r) *np.cos(theta_goal))
+          dely[i][j]  = (50 * (d_goal-r) *np.sin(theta_goal))
+          
+      if d_goal>r+s:
+        if delx[i][j] != 0:
+          delx[i][j] += 50* s *np.cos(theta_goal)
+          dely[i][j] += 50* s *np.sin(theta_goal)
+          print(dely[i][j])
+        else:
+          
+          delx[i][j] = 50* s *np.cos(theta_goal)
+          dely[i][j] = 50* s *np.sin(theta_goal) 
+      if d_goal<r:
+          delx[i][j] = 0
+          dely[i][j] = 0
+      
+
+  return delx,dely,obstacle,r
+
+
+
+def plot_graph(X, Y, delx, dely,obj, fig, ax, loc,r,i, color,start_goal=np.array([[0,0]])  ):
+  
+  ax.quiver(X, Y, delx, dely)
+  ax.add_patch(plt.Circle(loc, r, color=color))
+  ax.set_title(f'Robot path with {i} obstacles ')
+  ax.annotate(obj, xy=loc, fontsize=10, ha="center")
+  return ax
+
+for i in range(11):
+  fig, ax = plt.subplots(figsize = (10,10))
+
+  goal = [40,40]
+  delx, dely =add_goal(X,Y,s,r,goal)
+    
+  plot_graph(X, Y, delx, dely , 'Goal',fig, ax, goal, 7,0, 'b' )
+  obstacle_goal = [25,25] 
+  delx, dely, loc, r = add_obstacle(X,Y, delx,dely,obstacle_goal)
+  plot_graph(X, Y, delx, dely , 'Obstacle',fig, ax, obstacle_goal, r , 0,'m')
+    #ax.add_patch(plt.Circle(loc, 2, color='m'))
+  ax.streamplot(X,Y,delx,dely, start_points=seek_points,linewidth=4, cmap='autu')
+    
+  plt.show()
